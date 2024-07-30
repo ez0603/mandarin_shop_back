7ea29abc8847d346b7c1c40da8c2d2085f189bec
ez0603 <dlwldjs3132@naver.com>
2024-07-30 21:09:20 +0900

    first

---
+++ src/main/java/com/example/mandarin_shop_back/MandarinShopBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.mandarin_shop_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandarinShopBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MandarinShopBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mandarin_shop_back/MandarinShopBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mandarin_shop_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandarinShopBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
